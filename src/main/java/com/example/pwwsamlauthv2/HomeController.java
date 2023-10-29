93002fecc72d26bee9c992fb7efab4da8d1d0789
Ryan Olsen <ryan.olsen@portlandwebworks.com>
2023-10-29 18:57:46 -0400

    Used same dependency as the example to get Saml2AuthenticatedPrincipal to be valid

--- src/main/java/com/example/pwwsamlauthv2/HomeController.java
+++ src/main/java/com/example/pwwsamlauthv2/HomeController.java
@@ -1,20 +1,19 @@
 package com.example.pwwsamlauthv2;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.saml.SAMLAuthenticationProvider;
+import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @Controller
 public class HomeController {
 
   @RequestMapping("/")
-  public String home(@AuthenticationPrincipal SAMLAuthenticationProvider principal, Model model) {
-//    model.addAttribute("name", principal.getUserDetails());
-//    model.addAttribute("emailAddress", principal.getUserDetails());
-    model.addAttribute("userAttributes", principal.getUserDetails());
+  public String home(@AuthenticationPrincipal Saml2AuthenticatedPrincipal principal, Model model) {
+    model.addAttribute("name", principal.getName());
+    model.addAttribute("emailAddress", principal.getFirstAttribute("email"));
+    model.addAttribute("userAttributes", principal.getAttributes());
     return "home";
   }
 
